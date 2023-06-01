7da8fdb273d6732916ccadefbea0926b33e07c38
Szymon Leyk <biuro@szymonleyk.pl>
2023-06-01 14:53:26 +0200

    #9 add model and migration for mater

---
+++ database/migrations/1685623855993_create_maters_tables.ts
@@ -0,0 +1,19 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'maters'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('name').notNullable()
+      table.string('serial_number').unique().notNullable()
+      table.dateTime('installation_date').notNullable()
+      table.timestamps(true, true)
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
